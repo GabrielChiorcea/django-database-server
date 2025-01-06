8b894318e7a9032a6f4349847eeffaa59704ea64
Gabriel <gabrielchiorcea93@gmail.com>
2025-01-06 15:47:20 +0200

    Add detail view and URL routing for polls app

--- engine/polls/views.py
+++ engine/polls/views.py
@@ -5,4 +5,6 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
+def detail(request):
+    return HttpResponse("You're looking at question.")
 # Create your views here.
